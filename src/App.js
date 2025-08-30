cdb48a993600eb2e21b03033cdb39a359d9bfaef
ksowmya2482003 <your-bbhuvi525@gnmail.com>
2025-08-30 16:09:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>sowmya</h2>
+        sowmya
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
